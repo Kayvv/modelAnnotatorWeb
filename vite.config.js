bd22074722984549e33baa6b05dd9d66f73f1bd5
Kay <kayvvw@gmail.com>
2025-10-28 16:59:35 +1300

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   assetsInclude: ['**/*.wasm'],
+  base: process.env.NODE_ENV === 'production'
+    ? '/modelAnnotatorWeb/'
+    : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
