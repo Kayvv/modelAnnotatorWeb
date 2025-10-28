600622b7ee35f22db74921f6f2b3ec5153490e9a
Kay <kayvvw@gmail.com>
2025-10-28 17:09:47 +1300

    Try another deploy

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   assetsInclude: ['**/*.wasm'],
-  base: process.env.NODE_ENV === 'production'
-    ? '/modelAnnotatorWeb/'
-    : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  base: '/modelAnnotatorWeb/',
 })
